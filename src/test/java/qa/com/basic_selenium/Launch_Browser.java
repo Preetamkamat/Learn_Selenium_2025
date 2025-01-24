5d6575ba5964d0d8bfa9d60e2a94dae77eb66013
Preetamkamat <Preetamkamat1702@gmail.com>
2025-01-24 15:28:05 +0530

    Launch Browser Class

--- src/test/java/qa/com/basic_selenium/Launch_Browser.java
+++ src/test/java/qa/com/basic_selenium/Launch_Browser.java
@@ -1,4 +1,12 @@
 package qa.com.basic_selenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Launch_Browser {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "E:\\Learn\\Learn_Selenium_2025\\src\\main\\resources\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.in/");
+    }
 }
