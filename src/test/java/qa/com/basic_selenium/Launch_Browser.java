1198960557e2911c84fd824e69e2ede746bae962
Preetamkamat <Preetamkamat1702@gmail.com>
2025-01-24 19:39:08 +0530

    Updated Launch Browser

--- src/test/java/qa/com/basic_selenium/Launch_Browser.java
+++ src/test/java/qa/com/basic_selenium/Launch_Browser.java
@@ -2,11 +2,17 @@ package qa.com.basic_selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Wait;
 
 public class Launch_Browser {
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "E:\\Learn\\Learn_Selenium_2025\\src\\main\\resources\\drivers\\chromedriver.exe");
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "./src/main/resources/drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.in/");
+        Thread.sleep(50000);
+        driver.quit();
+
+
+
     }
 }
